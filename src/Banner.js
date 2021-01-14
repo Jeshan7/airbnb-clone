f1f600324ba04eb28316026aae601f8e2550c600
Jeshan <jeshankhn6@gmail.com>
2021-01-14 21:58:26 +0530

    Started airbnb-clone

---
+++ src/Banner.js
@@ -0,0 +1,29 @@
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import "./Banner.css";
+import Search from "./Search";
+
+function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          variant="outlined"
+          className="banner__searchButton"
+        >
+          Search Dates
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Getaaway</h1>
+        <h5>Plan</h5>
+        <Button variant="outlined">Explore Neaby</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
